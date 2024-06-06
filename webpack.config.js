03e52a792420b138769a6bdaa9169f07eeda65d7
Alexander <som17son25@gmail.com>
2024-06-06 18:54:50 +0300

    add Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 module.exports = (env) => {
   return  {
     mode: env.mode,
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'build'),
@@ -14,6 +14,18 @@ module.exports = (env) => {
     plugins: [
       new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
       new webpack.ProgressPlugin(),
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
 }
 }
