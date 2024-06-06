f88cc9e30f65c2b26b5f190662933efea93c7de0
Alexander <som17son25@gmail.com>
2024-06-06 18:28:41 +0300

    add env

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true
-  },
+module.exports = (env) => {
+  return  {
+    mode: env.mode,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true
+    },
+}
 }
